cb8ca3190c75f1aa8e5285814fdf605de64a641d
Kai Herget <slip.studios11@gmail.com>
2025-05-20 08:23:45 -0600

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to the Productivity App</h1>
-      <a href="/login">Login</a> | <a href="/signup">Sign Up</a>
-    </main>
-  );
-}
